c88327ccd3a0ae00d5ba644eb476b119c31d45a3
kee <keejuan05@gmail.com>
2018-08-23 15:46:30 -0700

    Add #render in #commence_proceedings and rename p->pos and v->val

--- sudoku.rb
+++ sudoku.rb
@@ -15,31 +15,31 @@ class SudokuGame
   end
 
   def retrieve_pos_from_ui
-    p = nil
-    until p && legal_illegibility_of_p?(p)
+    pos = nil
+    until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
 
       begin
-        p = parse_inanity(gets.chomp)
+        pos = parse_inanity(gets.chomp)
       rescue
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
-        p = nil
+        pos = nil
       end
     end
-    p
+    pos
   end
 
   def retrieve_value_from_ui
-    v = nil
-    until v && legal_illegibility_of_v?(v)
+    val = nil
+    until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
-      v = parse_insanity(gets.chomp)
+      val = parse_insanity(gets.chomp)
     end
-    v
+    val
   end
 
   def parse_inanity(string)
@@ -54,12 +54,15 @@ class SudokuGame
     pos_to_val(retrieve_pos_from_ui, retrieve_value_from_ui)
   end
 
-  def pos_to_val(p, v)
-    board[p] = v
+  def pos_to_val(pos, val)
+    board[pos] = val
   end
 
   def commence_proceedings
-    process_parameters until board_process_terminates?
+    until board_process_terminates?
+      @board.render
+      process_parameters
+    end
     puts "Congratulations, you win!"
   end
 
@@ -67,13 +70,13 @@ class SudokuGame
     board.terminate?
   end
 
-  def legal_illegibility_of_p?(pos)
+  def valid_pos?(pos)
     pos.is_a?(Array) &&
       pos.length == 2 &&
       pos.all? { |x| x.between?(0, board.size - 1) }
   end
 
-  def legal_illegibility_of_v?(val)
+  def valid_val?(val)
     val.is_a?(Integer) &&
       val.between?(0, 9)
   end
