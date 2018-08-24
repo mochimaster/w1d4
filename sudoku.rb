8207ccdba9d509ba461239ab0010da2b72f4a351
kee <keejuan05@gmail.com>
2018-08-23 17:11:44 -0700

    Find missing end

--- sudoku.rb
+++ sudoku.rb
@@ -109,7 +109,7 @@ class Tile
       @value = new_value
     end
   end
-
+end
 class SudokuGame
   def self.from_file(filename)
     board = Board.from_file(filename)
@@ -125,13 +125,13 @@ class SudokuGame
     until pos && valid_pos?(pos)
       puts "Please enter a position on the board (e.g., '3,4')"
       print "> "
-
+  
       begin
         pos = parse_pos(gets.chomp)
       rescue
         puts "Invalid position entered (did you use a comma?)"
         puts ""
-
+  
         pos = nil
       end
     end
@@ -147,15 +147,15 @@ class SudokuGame
     end
     val
   end
-
+  
   def parse_pos(string)
     string.split(",").map { |char| Integer(char) }
   end
-
+  
   def parse_val(string)
     Integer(string)
   end
-
+  
   def play_turn
     board.render
     pos = get_pos
